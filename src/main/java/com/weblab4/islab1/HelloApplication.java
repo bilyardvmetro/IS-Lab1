423743c0aea7766c1a6aaaf318f6529b58846fc4
Kirill Polenov <polenovkirill@gmail.com>
2025-10-04 17:27:10 +0300

    initial commit

---
+++ src/main/java/com/weblab4/islab1/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.weblab4.islab1;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
